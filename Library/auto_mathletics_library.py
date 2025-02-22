e1374917e5cccdf798d83de759d7c1254f0078ae
annpocoyo <ryant23@outlook.ie>
2025-02-22 20:00:13 +1000

    Automate "evaluation" type questions!

--- Library/auto_mathletics_library.py
+++ Library/auto_mathletics_library.py
@@ -7,7 +7,9 @@ AutoMathleticsClass - contains all functions used by automathletics.py
 """
 import time # For delays
 from _auto_browser_base import AutoBrowserBase # Base for automatic browser control
+from selenium.common.exceptions import *
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.remote.webelement import WebElement # For intelisense
 
 class AutoMathleticsClass(AutoBrowserBase):
@@ -16,3 +18,54 @@ class AutoMathleticsClass(AutoBrowserBase):
         # Initalize Class
         # First we need to contact our parent's constructer
         super().__init__(url = "https://studentportal.mathletics.com/")
+
+    def send_answer(self, answer):
+        """Types answer and moves to next question"""
+        # Get answer box
+        self.driver.switch_to.frame(0) # Switch to question IFrame
+        answer_box = self.driver.find_elements(By.CLASS_NAME, "input3p-focused")[0]
+
+        # Send answer and RETURN
+        answer_box.send_keys(answer)
+        self.driver.find_element(By.XPATH, "//html").send_keys(Keys.RETURN)
+        time.sleep(.15)
+        try:
+            self.driver.find_element(By.XPATH, "//html").send_keys(Keys.RETURN)
+        except NoSuchElementException:
+            # We've probably finished the question set. Ignore
+            pass
+        self.driver.switch_to.default_content() # Switch to root site
+
+    def wait_load(self):
+        """Wait for question to load."""
+        # Loop until answer box found.
+        answer_boxs = [] # To stop exception from throwing!
+        while len(answer_boxs) != 1:
+            try:
+                self.driver.switch_to.frame(0) # Switch to question IFrame
+                answer_boxs = self.driver.find_elements(By.CLASS_NAME, "input3p-focused")
+                self.driver.switch_to.default_content() # Switch to root site
+            except (StaleElementReferenceException, NoSuchFrameException):
+                # Not loaded yet
+                continue
+        self.driver.switch_to.default_content() # Switch to root site
+
+    @property
+    def quiz_finished(self):
+        """Return True if quiz finished, otherwise False."""
+        time.sleep(.15) # Give time for finish screen to show!
+        return len(
+            self.driver.find_elements(By.XPATH, "//*[contains(text(), 'Congratulations!')]")) > 0
+
+    @property
+    def current_equation(self):
+        """Current equation on display. Only works on equation type questions!"""
+        # TODO: Check for compatible question, if not compatible, throw an exception!
+        # Get current equation
+        self.driver.switch_to.frame(0) # Switch to question IFrame
+        value = self.driver.find_elements(By.CLASS_NAME, "question-text")[1].text
+        value = value.strip("=\n  ") # Post processing
+        value = value.replace("÷", "/").replace("×", "*") \
+        .replace("\n)", ")").replace("\n", "**") # DO NOT ASK ME HOW '\n' WORKS!
+        self.driver.switch_to.default_content() # Switch to root site
+        return value # Return equation
--- automathletics.py
+++ automathletics.py
@@ -18,8 +18,19 @@ from auto_mathletics_library import AutoMathleticsClass
 auto_mathletics = AutoMathleticsClass()
 
 def main():
-    # PLACEHOLDER
+    # We begin:
+    # Wait for user to confirm to have started activity.
+    print("Please sign into Mathletics and start your target activity.")
+    input("Press enter when you're ready:")
 
+    # Question loop
+    while not auto_mathletics.quiz_finished:
+        # TODO: Add support for more types of questions
+        auto_mathletics.wait_load() # Wait for question to load
+        answer = eval(auto_mathletics.current_equation) # Get & evaluate question
+        auto_mathletics.send_answer(answer) # Send in answer
+
+    print("Question set finished: Yay!!!")
     while True:
         pass
 
