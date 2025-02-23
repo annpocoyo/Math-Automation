20b5c36d5e43f616201de8622a5435a358cc0014
annpocoyo <ryant23@outlook.ie>
2025-02-23 10:15:04 +1000

    Add support for the basic 8 questions below word problem (doesn't do word problem)

--- Library/auto_my_imaths_library.py
+++ Library/auto_my_imaths_library.py
@@ -16,3 +16,30 @@ class AutoMyIMathsClass(AutoBrowserBase):
         # Initalize Class
         # First we need to contact our parent's constructer
         super().__init__(url = "https://app.myimaths.com/myportal/student/my_homework")
+
+    @property
+    def questions(self):
+        """Current set of questions"""
+        # Get all answer boxes
+        answer_boxes: list[WebElement] = []
+        for answer_box in self.driver.find_elements(By.XPATH, "//input"):
+            # Append answer box's parent's parent to the list (we need the div)
+            answer_boxes.append(answer_box.find_element(By.XPATH, "../../.."))
+
+        # Get all questions and question types into dictionary
+        questions: list[dict] = []
+        for answer_box in answer_boxes:
+            question = answer_box.find_element(By.XPATH, ".//preceding::div[1]").text \
+                .replace("×", "*").replace("÷", "/").replace("−", "-").replace("+", "+")
+            # Find question type
+            match question:
+                case x if "=" in x:
+                    question_type = "evaluation"
+                case _:
+                    question_type = None
+
+            # Append question to dict
+            questions.append({"question": question.strip(" ="), "answer_box": answer_box, "type": question_type})
+
+        # Return questinos
+        return questions
--- automyimaths.py
+++ automyimaths.py
@@ -19,13 +19,35 @@ auto_my_imaths = AutoMyIMathsClass()
 
 def main():
     # We begin:
+    # Store original window
+    original_window = auto_my_imaths.driver.current_window_handle
+
     # Wait for user to confirm to have started activity.
     print("Please sign into MyiMaths and start your target activity.")
     input("Press enter when you're ready:")
 
+    # Switch to new window
+    auto_my_imaths.driver.switch_to.window(auto_my_imaths.driver.window_handles[1])
+
+    # Switch to IFrame
+    auto_my_imaths.driver.switch_to.frame(auto_my_imaths.driver.find_element(By.XPATH, "//embed"))
+
     # Begin question loop
     while True: # TODO: Detect when questions finished
-        pass
+        # Loop through questions
+        for question in auto_my_imaths.questions:
+            # Check question type
+            match question["type"]:
+                case "evaluation":
+                    # Get and send question answer
+                    question["answer_box"].find_element(By.XPATH, ".//input").send_keys(eval(question["question"]))
+                case _:
+                    print("ERROR: Unknown question type detected: Skipping!")
+                    print("You will need to do the question yourself!")
+
+        # Finished current page!
+        print("Current page finished!")
+        input("Please move to the next page and then press enter:")
 
     input("Question set finished: Yay!!!:")
 
